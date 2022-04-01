025444d2c171c7195d66913ee76045a4c449599c
Christopher Ravosa <Christopher.ravosa1@marist.edu>
2022-04-01 16:20:24 -0400

    Added index.html

--- client/public/js/homepage.js
+++ client/public/js/homepage.js
@@ -1,5 +1,5 @@
-const element = (
-  <h1 className="greeting">
-    Hello, world!
-  </h1>
+const element = <h1>Hello, world</h1>;
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
 );
+root.render(element);
